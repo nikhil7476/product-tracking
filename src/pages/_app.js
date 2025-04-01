ca9b71d61090fc383dcf2a02ff4247520d07d382
nikhil7476 <nikhil9027917476@gmail.com>
2025-04-01 22:23:28 +0530

    modified:   src/pages/_app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,6 @@
-import { useEffect } from "react";
-import TagManager from "react-gtm-module";
-import "../styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const tagManagerArgs = {
-  gtmId: "GTM-5LK7ZQSF",
-};
-
-function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
-    console.log("✅ GTM Initialized:", tagManagerArgs.gtmId);
-  }, []);
-
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
